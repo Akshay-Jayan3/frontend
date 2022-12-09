9a871c916a3b407c0d863b9e4a547cd49f3bf797
Akshay_Jayan <akshayjayan321@gmail.com>
2022-12-09 16:25:54 +0530

    Add buttton to naviagte to add employee

---
+++ src/components/EmployeeMain.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react'
+import DateRangePicker from '@wojtekmaj/react-daterange-picker/dist/entry.nostyle'
+import { HiUserPlus } from "react-icons/hi2";
+import Employeedata from './Employeedata';
+
+const EmployeeMain = ({data ,setShow}) => {
+  const [value, onChange] = useState([new Date(), new Date()]);
+  const HandleAdd =()=>{
+    setShow(true)
+  }
+
+  return (
+    <div><div class="box">
+    <div className='addemployee'>
+        <div className='one'>
+            <h1>{data?.length}</h1>
+            <p>Employees</p>
+        </div>
+        <div className='two' onClick={HandleAdd}>
+          <p><span><HiUserPlus/></span>Add Employees</p>
+        </div>
+    
+    </div>
+    <div className='filter'>
+        <input id="search" placeholder='Search'/>
+        <select name="cars" id="cars">
+            <option value="volvo" className='option'>All Employees</option>
+            <option value="saab" className='option'>Managers</option>
+            <option value="mercedes" className='option'>salesman</option>
+            <option value="audi"className='option'>accountant</option>
+        </select>
+        <DateRangePicker style={{border:"1px solid #8991c0"}} onChange={onChange} value={value}  />
+       
+       
+    </div>
+    <div className='employeetable'>{data?.length !==0 ?<Employeedata data={data}/>:<div className='no-data'><h1>No employees</h1></div>}</div>
+
+    
+  </div></div>
+  )
+}
+
+export default EmployeeMain
--- src/components/Employees.js
+++ src/components/Employees.js
@@ -3,13 +3,15 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios'
 
 import '../styles/employeedata.css'
-import DateRangePicker from '@wojtekmaj/react-daterange-picker/dist/entry.nostyle'
-import { HiUserPlus } from "react-icons/hi2";
-import Employeedata from './Employeedata';
+
+
+import AddEmployee from './AddEmployee';
+import EmployeeMain from './EmployeeMain';
 
 const Employees = () => {
-  const [value, onChange] = useState([new Date(), new Date()]);
+
   const [data,setData]=useState(null)
+  const [show ,setShow]=useState(false)
 
 
   const url="http://192.168.2.74/employee/all";
@@ -29,7 +31,7 @@ const Employees = () => {
   useEffect(() => {
     getData();
     
-  }, [data])
+  }, [])
 
 
 
@@ -37,33 +39,18 @@ const Employees = () => {
 
 
   return (
-    <div class="box">
-    <div className='addemployee'>
-        <div className='one'>
-            <h1>{data?.length}</h1>
-            <p>Employees</p>
-        </div>
-        <div className='two'>
-          <p><span><HiUserPlus/></span>Add Employees</p>
-        </div>
-    
-    </div>
-    <div className='filter'>
-        <input id="search" placeholder='Search'/>
-        <select name="cars" id="cars">
-            <option value="volvo" className='option'>All Employees</option>
-            <option value="saab" className='option'>Managers</option>
-            <option value="mercedes" className='option'>salesman</option>
-            <option value="audi"className='option'>accountant</option>
-        </select>
-        <DateRangePicker style={{border:"1px solid #8991c0"}} onChange={onChange} value={value}  />
-       
-       
-    </div>
-    <div className='employeetable'><Employeedata data={data}/></div>
+
+    <>
+    {show ? <AddEmployee setShow={setShow}/>:<EmployeeMain data={data}  setShow={setShow}/>}
+    </>
+
+
+
+
+
+
 
     
-  </div>
   )
 }
 
